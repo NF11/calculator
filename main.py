0898f05d73470bcdde04b2a91d92582eed1c99bc
Nassim Fares <nassim.fares.dz@viacesi.fr>
2020-04-15 15:01:51 +0100

    done but can update

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import re
 
 print("calculator")
-print("type quit to exit")
+print("type quit or q to exit\n")
 prv = 0
 run = True
 
-def perform_math():
+
+def calculator():
+    global prv
+    global run
+    equation = ""
+    if prv == 0:
+        equation = input("enter equation , to use float type ',' :\n")
+    else:
+        equation = input(str(prv))
+    if equation == "quit" or equation == "q":
+        print("bye x)")
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z;.:()" "]', '', equation)
+        if prv == 0:
+            prv = eval(equation)
+        else:
+            prv = eval(str(prv) + equation)
+
+
+while run:
+    calculator()
